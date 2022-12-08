1abcc7fee680c1cc44527a516fe0025b2fd3a78d
PC-BAS\shnva <shnvanolphen@gmail.com>
2022-12-08 19:26:04 +0100

    Removed ElementCount, since its not needed anymore

--- VSRKMS/Models/RoomElement.cs
+++ VSRKMS/Models/RoomElement.cs
@@ -7,7 +7,6 @@ namespace VSRKMS.Models
         public string Guid { get; set; }
         public string Name { get; set; }
         public int ElementId { get; set; }
-        public int Count { get; set; }
         public List<Fault> Faults { get; set; }
     }
 }
