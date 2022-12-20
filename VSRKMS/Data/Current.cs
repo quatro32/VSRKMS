01a665d330b4b38ffc5847d710c32e4f1468eb24
PC-BAS\shnva <shnvanolphen@gmail.com>
2022-12-20 15:58:13 +0100

    Removed sessionstorage and replaced with singleton instace when managing state management

---
+++ VSRKMS/Data/Current.cs
@@ -0,0 +1,27 @@
+﻿using VSRKMS.Models;
+
+namespace VSRKMS.Data
+{
+    public class Current
+    {
+        private static Current _instance;
+
+        private Current() { }
+
+        public static Current Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Current();
+
+                return _instance;
+            }
+        }
+
+        public Audit Audit { get; set; }
+        public AuditCategory AuditCategory { get; set; }
+        public RoomElement RoomElement { get; set; }
+        public Room Room { get; set; }
+    }
+}
